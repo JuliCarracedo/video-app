469b89f047ba09634a45afc43a3b86b6e057c846
JuliCarracedo <julicarra@hotmail.com>
2021-10-21 12:42:57 -0300

    Add homepage and navbar components

---
+++ src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Homepage = () =>(
+
+    <div id="homepage">
+        <section>
+            <h2>Recommended</h2>
+        </section>
+        <section>
+            <h2>Sports</h2>
+        </section>
+        <section>
+            <h2>News</h2>
+        </section>
+    </div>
+
+);
