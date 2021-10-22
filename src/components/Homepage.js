11268c77080175483cef79b1615034b9506bf585
JuliCarracedo <julicarra@hotmail.com>
2021-10-22 16:46:37 -0300

    Add nabvar styles and side menues

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import './homepage.css';
 
 const Homepage = () => (
-
   <div id="homepage">
-    <section>
-      <h2>Recommended</h2>
-    </section>
-    <section>
-      <h2>Sports</h2>
-    </section>
-    <section>
-      <h2>News</h2>
-    </section>
+    <div id="leftmenu">
+      leftMenu
+    </div>
+    <div id="main">
+      <section>
+        <h2>Recommended</h2>
+      </section>
+      <section>
+        <h2>Sports</h2>
+      </section>
+      <section>
+        <h2>News</h2>
+      </section>
+    </div>
+    <div id="rightmenu">
+      rightmenu
+    </div>
   </div>
 
 );
